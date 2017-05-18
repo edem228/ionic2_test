783a056d06e3c1974d2913d101a234acd768185b
Edem <edem228@gmail.com>
2017-05-18 15:59:06 +0200

    Add ionic native Geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  location:any;
+  platform:any;
+  constructor(public navCtrl: NavController, private geolocation: Geolocation, platform: Platform) {
+    this.platform = platform
+    platform.ready().then(() => {
 
-  constructor(public navCtrl: NavController) {
+      // get current position
+      geolocation.getCurrentPosition().then(pos => {
+        console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+      });
 
+      const watch = geolocation.watchPosition().subscribe(pos => {
+        console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+      });
+
+      // to stop watching
+      watch.unsubscribe();
+
+    });
+
+    }
   }
-
-}
+//   onLocateUser() {
+//     Geolocation.getCurrentPosition()
+//       .then(
+//       (location) => {
+//         console.log('Location successful')
+//         this.location.lat = location.coords.latitude;
+//         this.location.lng = location.coords.longitude;
+//       }
+//       )
+//     }
+// }
